07d67cff27b0dbbad1437066fe4126c89d71cc05
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-20 16:01:21 -0700

    create basic controller and template

--- src/main/java/com/ktill/queuesaber/controllers/IndexController.java
+++ src/main/java/com/ktill/queuesaber/controllers/IndexController.java
@@ -1,4 +1,20 @@
 package com.ktill.queuesaber.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Splash screen/index controller. This is the first page a user will see and it will allow them to set their username
+ */
+@Controller
 public class IndexController {
+
+    //TODO: Check cookie for existing username
+
+    //TODO: Set cookie after username is entered
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String getIndex(){
+        return "index";
+    }
 }
