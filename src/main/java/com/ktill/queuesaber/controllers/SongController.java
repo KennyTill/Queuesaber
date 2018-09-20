2b11fdb663463f8b503cd1e5a7c129844024fd17
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-20 16:38:53 -0700

    creating song list endpoint

--- src/main/java/com/ktill/queuesaber/controllers/SongController.java
+++ src/main/java/com/ktill/queuesaber/controllers/SongController.java
@@ -1,8 +1,15 @@
 package com.ktill.queuesaber.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class SongController {
 
+    @GetMapping("/all")
+    public String getSongs(){
+        return "songs";
+    }
 
 
 }
