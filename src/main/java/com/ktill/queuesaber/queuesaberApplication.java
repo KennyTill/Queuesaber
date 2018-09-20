e2f9798b8078081808fee5838bcf0c596d9a480d
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-20 15:22:47 -0700

    Inital commit

---
+++ src/main/java/com/ktill/queuesaber/queuesaberApplication.java
@@ -0,0 +1,11 @@
+package com.ktill.queuesaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class queuesaberApplication {
+    public static void main(String[] args){
+        SpringApplication.run(queuesaberApplication.class, args);
+    }
+}
