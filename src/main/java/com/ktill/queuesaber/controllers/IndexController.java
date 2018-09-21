37c94d61ba68ac68dc3dca822b52360dafcb800a
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-20 20:46:51 -0700

    updating for basic routing with cookie

--- src/main/java/com/ktill/queuesaber/controllers/IndexController.java
+++ src/main/java/com/ktill/queuesaber/controllers/IndexController.java
@@ -4,6 +4,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Splash screen/index controller. This is the first page a user will see and it will allow them to set their username
@@ -12,10 +17,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping({"", "/", "index", "index.html"})
-    public String getIndex(@CookieValue(name = "queuesaberCookie", value = "playerName", required = false) String playerName){
-        if (StringUtils.isBlank(playerName)) {
+    public String getIndex(@CookieValue(name = "queuesaberCookie"
+            , defaultValue = StringUtils.EMPTY) String queuesaberCookie){
+
+        if (StringUtils.isBlank(queuesaberCookie)) {
+            //cookie isn't set, show index template
             return "index";
         }
-        return "songs";
+
+        return "redirect:/all";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(@RequestParam("userName") String userName, HttpServletResponse response){
+        //set a cookie here with the user's display name
+        response.addCookie(new Cookie("queuesaberCookie", userName));
+        return "redirect:/all";
     }
 }
--- src/main/java/com/ktill/queuesaber/controllers/SongController.java
+++ src/main/java/com/ktill/queuesaber/controllers/SongController.java
@@ -1,13 +1,19 @@
 package com.ktill.queuesaber.controllers;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SongController {
 
     @GetMapping("/all")
-    public String getSongs(){
+    public String getSongs(@CookieValue(value = "queuesaberCookie", defaultValue = StringUtils.EMPTY) String queuesaberCookie){
+        if (StringUtils.isBlank(queuesaberCookie)){
+            //cookie not set, redirect to index page
+            return "redirect:/index";
+        }
         return "songs";
     }
 
