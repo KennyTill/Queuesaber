f10ab921757f865f318622b75263c9ad9a4f6fd9
Kenneth Till <kenneth.g.till@gmail.com>
2018-09-20 21:05:11 -0700

    begin work on data loader

---
+++ src/main/java/com/ktill/queuesaber/components/DataLoader.java
@@ -0,0 +1,9 @@
+package com.ktill.queuesaber.components;
+
+import com.ktill.queuesaber.models.SongModel;
+
+import java.util.List;
+
+public interface DataLoader {
+    List<SongModel> loadSongs();
+}
---
+++ src/main/java/com/ktill/queuesaber/components/JSONDataLoader.java
@@ -0,0 +1,12 @@
+package com.ktill.queuesaber.components;
+
+import com.ktill.queuesaber.models.SongModel;
+
+import java.util.List;
+
+public class JSONDataLoader implements DataLoader {
+
+    public List<SongModel> loadSongs() {
+        return null;
+    }
+}
